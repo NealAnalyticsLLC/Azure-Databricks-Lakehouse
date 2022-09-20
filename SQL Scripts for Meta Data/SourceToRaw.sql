955036ff56e3c8d518bb8cdd6cb79c6f52297278
PiyushP1503 <78087497+PiyushP1503@users.noreply.github.com>
2022-09-20 11:33:31 +0530

    Update SourceToRaw.sql

--- SQL Scripts for Meta Data/SourceToRaw.sql
+++ SQL Scripts for Meta Data/SourceToRaw.sql
@@ -1,21 +1,3 @@
-/************************************************************************** 
-* Copyright (c) NEAL ANALYTICS LLC 2018
-* __________________
-* All Rights Reserved.
-* 
-* NOTICE:  All information contained herein is, and remains the property of 
-* Neal Analytics LLC.
-* 
-* The intellectual and technical concepts contained herein are proprietary to 
-* Neal Analytics LLC and may be covered by U.S. and Foreign Patents, patents 
-* in process, and are protected by trade secret or copyright law.
-* 
-* Dissemination of this information or reproduction of this material is 
-* strictly forbidden unless prior written permission is obtained from Neal 
-* Analytics LLC.
-*****************************************************************************/
-
-/****************************************************************************/
 CREATE TABLE [dbo].[SourceToRaw](
 	[ServerName] [varchar](50) NOT NULL,
 	[DatabaseName] [varchar](50) NOT NULL,
